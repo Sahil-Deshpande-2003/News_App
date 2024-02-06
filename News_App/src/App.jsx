36bd9e2df00f5a13720a9484dbe750a268310b92
Sahil Deshpande <sahsdep@gmail.com>
2024-02-06 11:08:37 +0530

    Spinner component and routes working category is changing using propes

--- News_App/src/App.jsx
+++ News_App/src/App.jsx
@@ -1,17 +1,50 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import News from './components/News'
 import Navbar from './components/Navbar'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 export class App extends Component {
   static propTypes = {
 
   }
 
+  /*
+
+  business
+entertainment
+general
+health
+science
+sports
+technology
+  */
+
   render() {
+    console.log("Inside render")
     return (
       <div>
+        
+        <Router>
+          {/* My mistake was that I had kept Navbar outside Router, it should be inside Router */}
         <Navbar/>
-        <News/>
+        <Routes>
+          {/* problem is navbar pe dusre catgeory pe click karne par component remount nahi ho raha hai naye props ke saath, uske liye har News component ko ek unique key deni padegi */}
+
+          <Route exact path="/" element={<News pageSize={5}  key="general" country="in" apiKey="a88e1692409d4c84ab688af0c5eada56" category="general" />}/>
+          <Route exact path="/business" element={<News pageSize={5}  key="business" country="in" apiKey="a88e1692409d4c84ab688af0c5eada56" category="business" />}/>
+          <Route exact path="/entertainment"  element={<News pageSize={5}  key="entertainment" country="in" apiKey="a88e1692409d4c84ab688af0c5eada56" category="entertainment" />}/>
+          <Route exact path="/health"  element={<News pageSize={5} country="in" key="health"apiKey="a88e1692409d4c84ab688af0c5eada56" category="health" />}/>
+          <Route exact path="/science" key="science" element={<News pageSize={5} country="in" apiKey="a88e1692409d4c84ab688af0c5eada56" category="science" />}/>
+          <Route exact path="/sports"  element={<News pageSize={5} country="in" key="sports"apiKey="a88e1692409d4c84ab688af0c5eada56" category="sports" />}/>
+          <Route exact path="/technology"  element={<News pageSize={5} country="in" key="technology"apiKey="a88e1692409d4c84ab688af0c5eada56" category="technology" />}/>
+         
+         
+           
+        </Routes>
+        </Router>
       </div>
     )
   }
--- News_App/src/components/Newsitem.jsx
+++ News_App/src/components/Newsitem.jsx
@@ -8,7 +8,12 @@ export class Newsitem extends Component {
 
 
 
-  
+  constructor(){
+
+    super();
+
+    console.log("Inside Newsitem constructor")
+  }
         
 
   render() {
@@ -16,11 +21,11 @@ export class Newsitem extends Component {
     return (
       <div>
         <div className="card">
-  <img src={this.props.imageUrl} className="card-img-top" alt="..."/>
+  <img src={this.props.imageUrl!=null?this.props.imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_icnonsILxOwjnB_JigBAq9s_l0WcRZ6DVw&usqp=CAU"} className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">{this.props.title}</h5>
-    <p className="card-text">{this.props.description}</p>
-    <a href={this.props.url} className="btn btn-primary" target= "_blank">Read More</a>
+    <h5 className="card-title">{this.props.title!=null?this.props.title:"Unknown"}</h5>
+    <p className="card-text">{this.props.description!=null?this.props.description:"Unknown"}</p>
+    <a href={this.props.url!=null?this.props.url:""} className="btn btn-primary" target= "_blank">Read More</a>
   </div>
 </div>
       </div>
